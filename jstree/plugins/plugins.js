c26ed167bd769da8bf162cf1ac4f8603d67f3609
Hauke Mewes <haukemewes@googlemail.com>
2012-05-20 15:20:28 +0200

    added vendor files

---
+++ jstree/plugins/plugins.js
@@ -0,0 +1,16 @@
+// At the moment, this is a static file
+// Later on it will be editable by a PHP script
+window.Plugins = {
+    "usermanager": {
+        js: "jstree/app/usermanager",
+        rights: 2,
+        route: "usermanager"
+    },
+    
+    // Only for testing
+    /*"filetester": {
+        js: "jstree/filetester",
+        rights: 0,
+        route: "filetester"
+    }*/    
+};
