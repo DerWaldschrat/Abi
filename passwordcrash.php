7d034df32f70c6b8f4d497beda9479085b25471a
Hauke Mewes <haukemewes@googlemail.com>
2012-06-18 21:08:32 +0200

    added hasher for forgotten pws

---
+++ passwordcrash.php
@@ -0,0 +1,25 @@
+﻿<?php
+    $hash = "";
+    require "hasher.php";
+    if (isset($_POST["nick"]) && isset($_POST["pass"])) {
+        $hash = hashme($_POST["pass"], $_POST["nick"]);
+    }
+?><!DOCTYPE html>
+<html>
+
+<head>
+    <title>Passwortcrash</title>
+
+    <meta charset="UTF-8" />
+
+</head>
+
+<body>
+    <form action="passwordcrash.php" method="post">
+    <label for="nick">Nickname:</label><input type="text" id="nick" name="nick" /><br />
+    <label for="pass">Passwort:</label><input type="text" id="pass" name="pass" /><br />
+    <input type="submit" value="Generiere Hash" /><br />
+    </form>
+    <?php echo $hash; ?>
+</body>
+</html>
