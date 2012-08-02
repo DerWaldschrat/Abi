133d01dbf543063bd948fd364fb71097d82b518e
Hauke Mewes <haukemewes@googlemail.com>
2012-08-02 16:44:03 +0200

    made session generate a new id every hour

--- session.php
+++ session.php
@@ -1,6 +1,18 @@
 <?php
+// starts the session
 session_start();
 define("SESSION_INIT", true);
+// sets session initialization time if not set
+if (!(isset($_SESSION["__init"]))) {
+    $_SESSION["__init"] = time();
+// checks if session is older than an hour, then change session id
+} else {
+    if ($_SESSION["__init"] < (time() - 3600) ) {
+        session_regenerate_id();
+        $_SESSION["__init"] = time();
+    }
+}
+
 function isLoggedin($required = 0) {
     return isset($_SESSION["user"]["loggedin"]) && $_SESSION["user"]["loggedin"] === true && $_SESSION["user"]["rights"] >= $required;
 }
