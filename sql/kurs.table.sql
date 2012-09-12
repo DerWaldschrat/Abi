a15890cd50fb1e8baef77535a84a46471cfbecfd
Hauke Mewes <haukemewes@googlemail.com>
2012-09-12 18:09:32 +0200

    added sql for new table kurs

---
+++ sql/kurs.table.sql
@@ -0,0 +1,8 @@
+-- Tabelle, um alle Kurse zu speichern
+CREATE TABLE abi_kurs (
+  kursid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  kuerzel VARCHAR(10),
+  lehrer VARCHAR(80),
+  fach VARCHAR(40),
+  stunden VARCHAR(40)
+)
