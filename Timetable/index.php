9d77737b103ecad212e681e1e3038a83404e446d
Hauke Mewes <haukemewes@googlemail.com>
2012-09-12 18:42:12 +0200

    added working Timetable backend - creating and receiving lessons is allowed

--- Timetable/index.php
+++ Timetable/index.php
@@ -1,11 +1,51 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: hauke
- * Date: 12.09.12
- * Time: 18:06
- * To change this template use File | Settings | File Templates.
- */
-
+define("__EXEC", true);
+define("IN", "../");
+define("PHP_EX", ".php");
+require IN . "coreconfig" . PHP_EX;
+require IN . "mapper" . PHP_EX;
+require IN . "validator" . PHP_EX;
+if (isLoggedin(1)) {
+    // Creating new lessons
+    post(function () {
+        $lesson = bodyAsJSON();
+        if (hasAllSet($lesson, array("kuerzel, lehrer, fach, stunden")) && is_array($lesson->stunden)) {
+            $valid = true;
+            foreach($lesson->stunden as $stunde) {
+                if (!is_int($stunde)) {
+                    $valid = false;
+                    break;
+                }
+            }
+            if ($valid === true) {
+                $db = db();
+                $st = $db->prepare("INSERT INTO " . KURS . " (kuerzel, lehrer, fach, stunden, fromid)");
+                $st->bind_param("sssss", $lesson->kuerzel, $lesson->lehrer, $lesson->fach, $lesson->stunden, userField("userid"));
+                if (exQuery($st)) {
+                    hJSON(array("kursid" => $db->insert_id));
+                } else {
+                    fail("lessonSaveFail");
+                }
+            } else {
+                fail("lessonSaveFail");
+            }
+        } else {
+            fail("lessonSaveFail");
+        }
+    });
+    get(function () {
+        $db = db();
+        $query = $db->query("SELECT kursid, kuerzel, lehrer, fach, stunden FROM " .KURS);
+        if ($query instanceof mysqli_result) {
+            $results = array();
+            while ($row = $query->fetch_object()) {
+                $results[] = $row;
+            }
+            hJSON($results);
+        }
+    });
+} else {
+    h404();
+}
 
 ?>
