2a365c5207025a1272d9997e363e45f3b4529bc5
Hauke Mewes <haukemewes@googlemail.com>
2012-10-09 21:13:50 +0200

    created little stresstest which is deactivated in production, of course

--- Calendar/index.php
+++ Calendar/index.php
@@ -8,6 +8,9 @@ require IN . "mapper" . PHP_EX;
 if (isLoggedin(1)) {
     // Get all terms in given interval
     get(function () {
+       /* hJSON();
+        echo file_get_contents("stress.json");
+        exit;*/
         $dateRegex = "/[0-9]{4,4}-[0-1][0-9]-[0-3][1-9]/";
         // if it is valid date, we will (probably) not raise an sql error for that
         if (isset($_GET["from"]) && preg_match($dateRegex, $_GET["from"]) == 1 && isset($_GET["to"]) && preg_match($dateRegex, $_GET["to"]) == 1) {
