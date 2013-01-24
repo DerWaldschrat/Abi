18dbfb77435a28a18752043f54b6cc018d93aaf8
DerWaldschrat <haukemewes@googlemail.com>
2013-01-24 18:50:38 +0100

    added construction.php

---
+++ design/construction.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!doctype html>
+<html>
+    <head>
+        <title>Wartungsarbeiten</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        Aufgrund von Wartungsarbeiten ist diese Seite momentan nicht verfügbar. <br />
+        Ihr bleibt jedoch hoffentlich eingeloggt.<br />
+        Vielen Dank für euer Verständnis.
+    </body>
+</html>
