2e4aa99347312b30f0d1421ff8b29e87ebcbd571
DerWaldschrat <haukemewes@googlemail.com>
2013-02-16 16:14:57 +0100

    updated data fetching to get lessons

--- User/fetchData.php
+++ User/fetchData.php
@@ -11,9 +11,9 @@ defined("__EXEC") or exit;
  */
 function fetchUserData(&$user)
 {
-    $st = db()->prepare("SELECT geburtstag, strasse, wohnort, danksagung, positiv, negativ, zukunft, semi_thema, p_semi, w_semi, abi_schriftlich, abi_muendlich_1, abi_muendlich_2 FROM " .USER_DATA . " WHERE userid = ?");
+    $st = db()->prepare("SELECT geburtstag, strasse, wohnort, danksagung, positiv, negativ, zukunft, semi_thema, p_semi, w_semi, abi_schriftlich, abi_muendlich_1, abi_muendlich_2, german, math FROM " .USER_DATA . " WHERE userid = ?");
     $st->bind_param("i", $user->userid);
-    $st->bind_result( $user->geburtstag, $user->strasse, $user->wohnort, $user->danksagung, $user->positiv, $user->negativ, $user->zukunft, $user->semi_thema, $user->p_semi, $user->w_semi, $user->abi_schriftlich, $user->abi_muendlich_1, $user->abi_muendlich_2);
+    $st->bind_result( $user->geburtstag, $user->strasse, $user->wohnort, $user->danksagung, $user->positiv, $user->negativ, $user->zukunft, $user->semi_thema, $user->p_semi, $user->w_semi, $user->abi_schriftlich, $user->abi_muendlich_1, $user->abi_muendlich_2, $user->german, $user->math);
     $st->execute();
     $st->fetch();
     $user->geburtstag = dateChangeFromISO($user->geburtstag);
